a9e6d9a9ac6076cee05f864417d02dc863f3d45b
Tutu1993 <625706468@qq.com>
2017-10-21 01:43:44 +0800

    change style

--- app/config/database.php
+++ app/config/database.php
@@ -1,9 +1,9 @@
 <?php
-return array(
+return [
     'database_type' => 'mysql',
     'database_name' => 'test',
     'server' => 'localhost',
     'username' => 'root',
     'password' => 'Shan0720',
     'charset' => 'utf8'
-);
+];
