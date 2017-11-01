de56eadc48d6f853e74c36e48936a70331779299
Pama <625706468@qq.com>
2017-11-01 18:23:47 +0800

    news.html

--- app/ctrl/newsCtrl.php
+++ app/ctrl/newsCtrl.php
@@ -22,4 +22,44 @@ class newsCtrl extends Core
         $this->assign($data);
         $this->display('news.html');
     }
+
+	public function view()
+    {
+        // $database = new Database('info');
+		$page = isset($_GET['page']) ? $_GET['page'] : 1;
+		$data = [
+			'title' => '凯融视角',
+			'type' => 'view',
+			'D_page' => 9,
+			'page' => $page
+		];
+        $this->assign($data);
+        $this->display('news_list.html');
+    }
+
+	public function case()
+    {
+        // $database = new Database('info');
+		$data = [
+			'title' => '案例分析',
+			'type' => 'case',
+			'D_page' => 4,
+			'page' => 0
+		];
+        $this->assign($data);
+        $this->display('news_list.html');
+    }
+
+	public function event()
+    {
+        // $database = new Database('info');
+		$data = [
+			'title' => '凯融活动',
+			'type' => 'event',
+			'D_page' => 4,
+			'page' => 0
+		];
+        $this->assign($data);
+        $this->display('news_list.html');
+    }
 }
